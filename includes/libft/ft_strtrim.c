579593b1d7d23e2c6e3a2b0cba67376d46612ad1
Sebastian Miro <smiro@cbr12s3.42barcelona.com>
2023-03-21 13:11:06 +0100

    Libft

--- includes/libft/ft_strtrim.c
+++ includes/libft/ft_strtrim.c
@@ -16,16 +16,13 @@ char	*ft_strtrim(char const *s1, char const *set)
 {
 	size_t	i;
 	size_t	j;
-	size_t	e;
 
-	e = 1;
 	i = 0;
 	j = ft_strlen(s1) - 1;
 	while (s1[i] && ft_strchr(set, s1[i]))
 		i++;
 	if (i > j)
 		return (ft_substr(s1, i, (j - i + 1)));
-	e = 1;
 	while (s1[j] && ft_strchr(set, s1[j]))
 		j--;
 	return (ft_substr(s1, i, (j - i + 1)));
