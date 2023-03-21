9382460065ea946f1b99a738591e9c20c3dcc1c4
Sebastian Miro <smiro@cbr12s3.42barcelona.com>
2023-03-21 12:57:50 +0100

    Fractol 2.0

--- src/draw_controls.c
+++ src/draw_controls.c
@@ -24,15 +24,20 @@ int	darker(int color, int y)
 	return (0 << 24 | (r - r / 2 + y * 3) << 16 | (g - g / 2 + y * 3) << 8 | (b - b / 2 + y * 3));
 }
 
+static void	put_text_justified(t_fractol *fractol, int w, int h, char *str)
+{
+	mlx_string_put(fractol->mlx.mlx, fractol->mlx.win, w + ft_strlen(str) / 2, h, 0x00EBE0ED, str);
+}
+
 void	put_text(t_fractol *fractol)
 {
 	int	n = W / 7;
-	mlx_string_put(fractol->mlx.mlx, fractol->mlx.win, n, H - 16, 0x00EBE0ED, "Move: Arrows");
-	mlx_string_put(fractol->mlx.mlx, fractol->mlx.win, n * 2, H - 16, 0x00EBE0ED, "Zoom: scroll up/down");
-	mlx_string_put(fractol->mlx.mlx, fractol->mlx.win, n * 3, H - 16, 0x00EBE0ED, "Change patron: p");
-	mlx_string_put(fractol->mlx.mlx, fractol->mlx.win, n * 4, H - 16, 0x00EBE0ED, "Reset: r");
-	mlx_string_put(fractol->mlx.mlx, fractol->mlx.win, n * 5, H - 16, 0x00EBE0ED, "More/Less iterations: +/-");
-	mlx_string_put(fractol->mlx.mlx, fractol->mlx.win, n * 6, H - 16, 0x00EBE0ED, "Exit: esc");
+	put_text_justified(fractol, n, H - 16, "Move: Arrows");
+	put_text_justified(fractol, n * 1.75, H - 16, "Zoom: scroll up/down");
+	put_text_justified(fractol, n * 2.80, H - 16, "Change patron: p");
+	put_text_justified(fractol, n * 3.75, H - 16, "More/Less iterations: +/-");
+	put_text_justified(fractol, n * 5.25, H - 16, "Reset: r");
+	put_text_justified(fractol, n * 6, H - 16, "Exit: esc");
 	mlx_string_put(fractol->mlx.mlx, fractol->mlx.win, 10, H - 16, 0x00333333, fractol->set);
 	if (fractol->set[0] == 'j')
 		mlx_string_put(fractol->mlx.mlx, fractol->mlx.win, 20, 20, 0x00523B56, "Press: j");
